2b80c3d0fa1b4a60ab7969c7faf4f43b3bd9aa51
crickett <crickettcode@gmail.com>
2018-03-27 18:52:09 -0400

    for loop

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ $(".btn-letter").click(function () {
     $(this).hide()
     console.log($(this))
     console.log(event.target.innerText)
-    if (event.target.innerText === 'N' || event.target.innerText == 'E' || event.target.innerText == 'R' || event.target.innerText == 'D') {
+    if var word = ['d', 'o', 'g', 'g', 'i', 'e']
+    var arryLength = word.length
+
+    for count = 0; count < arryLength i++
+
+
+   // (event.target.innerText === 'N' || event.target.innerText == 'E' || event.target.innerText == 'R' || event.target.innerText == 'D') {
         alert("That letter is in the word!")
-    } else {
-        alert("That letter is not in the word stupid!")
-        $("#lostBox").append("<p>" + event.target.innerText + "</p>")
+} else {
+    alert("That letter is not in the word stupid!")
+        $("#lostBox").append('<button type="button" class="btn-letter btn btn-outline-warning">' + event.target.innerText + '</button>')
     }
 });
 
